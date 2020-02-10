4cb2596f731305ecf9f536545baa0442220b53ce
Thadchet <b.bosskungzz@gmail.com>
2020-02-10 10:26:23 +0700

    create account

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,34 @@
+import { RouterModule ,Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule }   from '@angular/forms';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { CreateFormComponent } from './create-form/create-form.component';
 
+const appRoutes: Routes = [
+  { path: "", component: LoginFormComponent },
+  { path: "register",  component: CreateFormComponent },
+  
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    CreateFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
-
+    BrowserAnimationsModule,
+    FormsModule ,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
