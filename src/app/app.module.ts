36ebdfe9feec0ddb585590a2655163a0766f3646
Boss <wutipong1234@hotmail.com>
2020-02-17 16:13:58 +0700

    Add car page with navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarComponent } from './car/car.component';
 import { BoxComponent } from './box/box.component';
+import { PopUpComponent } from './pop-up/pop-up.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarComponent,
-    BoxComponent
+    BoxComponent,
+    PopUpComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
