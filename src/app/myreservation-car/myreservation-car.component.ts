d79e6f4131b23f6adeaa15a5ab5c1c01cd772d7e
Thadchet <sogood_boss@hotmail.com>
2020-03-11 00:13:13 +0700

    add state button & change review

--- src/app/myreservation-car/myreservation-car.component.ts
+++ src/app/myreservation-car/myreservation-car.component.ts
@@ -1,5 +1,5 @@
 import { REVIEWS } from '../review/mock-review';
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { ReviewComponent } from '../review/review.component';
 
 
@@ -11,7 +11,9 @@ import { ReviewComponent } from '../review/review.component';
 export class MyReservationCarComponent implements OnInit {
   reviews = REVIEWS ;
   @Input() car ;
-
+  constructor(private elem: ElementRef) {
+    
+  }
   review_popup(){
     document.getElementById('review_popup').className = "modal modal-fx-3dFlipVertical is-active" ;
   }
@@ -21,11 +23,18 @@ export class MyReservationCarComponent implements OnInit {
   agreement_popup(){
     document.getElementById('agreement_popup').className = "modal modal-fx-3dFlipVertical is-active" ;
   }
-  constructor() {
-    
-   }
-
+  
   ngOnInit() {
+    if(this.car.status == "Canceled"){
+      this.elem.nativeElement.querySelectorAll('button')[1].className = 'button is-danger is-disabled'
+      this.elem.nativeElement.querySelectorAll('button')[2].className = 'button is-info is-disabled'
+      this.elem.nativeElement.querySelectorAll('button')[3].className = 'button is-dark is-disabled'
+    }else if(this.car.status == "Returned"){
+      this.elem.nativeElement.querySelectorAll('button')[1].className = 'button is-danger is-disabled'
+    }else{
+      this.elem.nativeElement.querySelectorAll('button')[0].id = "pickup"
+      this.elem.nativeElement.querySelectorAll('button')[3].className = 'button is-dark is-disabled'
+    }
   }
 
 }
--- src/app/myreservation-popup/myreservation-popup.component.ts
+++ src/app/myreservation-popup/myreservation-popup.component.ts
@@ -13,12 +13,7 @@ export class MyreservationPopupComponent implements OnInit {
     
   }
   ngAfterViewInit(){
-    var arr = Array.from(document.getElementsByClassName('fa'))
-    for(let j = 0 ; j < this.reviews.length ; j++){
-       for(let i = 0 ; i < this.reviews[j].rating ; i++){
-         arr[5*j+i].className = "fa fa-star checked"     
-      }
-    }
+    
   }
   delete_review_popup(){
     document.getElementById('review_popup').className = "modal modal-fx-3dFlipVertical" ;
