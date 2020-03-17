5f5a9a7121c16c1beb945ef3eaf3303480b6b600
Thadchet <sogood_boss@hotmail.com>
2020-03-17 18:12:20 +0700

    up

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import axios from 'axios';
 import { Router } from '@angular/router';
@@ -22,20 +22,43 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
-   
+   console.log(Array.prototype.slice.call(document.getElementsByTagName("html"))[0])
   }
 
   onSubmit(){
-   console.log(this.loginForm.value)
-   var res =  this.authService.login(this.loginForm.value)
-   
-    // if(res === "200"){
-    //   this.router.navigate(['/homepage'])
-    // }else if(res ==="401"){
-    //   alert("Incorrect username or password")
-    // }
+    if((this.loginForm.value.username == null || this.loginForm.value.username == "" )&& (this.loginForm.value.password == null ||this.loginForm.value.password == "")){
+      console.log("username password null")
+      document.getElementsByClassName("input")[0].className = "input is-danger"
+      document.getElementsByClassName("input")[1].className = "input is-danger"
+      document.getElementsByClassName("non-active")[0].className = "active"
+    }else if(this.loginForm.value.username == null || this.loginForm.value.username == ""){
+      console.log("username null")
+      document.getElementsByClassName("input")[0].className = "input is-danger"
+      document.getElementsByClassName("non-active")[0].className = "active"
+    }else if(this.loginForm.value.password == null || this.loginForm.value.password == ""){
+      console.log("password null")
+      document.getElementsByClassName("input")[1].className = "input is-danger"
+      document.getElementsByClassName("non-active")[0].className = "active"
+    }else {
+      console.log("complete")
+      console.log(this.loginForm.value)
+      var res =  this.authService.login(this.loginForm.value)
+      this.router.navigate(['/homepage'])
+    }
   }
 
+  usernameChange(){
+    document.getElementsByClassName("input")[0].className = "input"
+  }
+  passwordChange(){
+    document.getElementsByClassName("input")[1].className = "input"
+  }
 
+  hiddenError(){
+    document.getElementsByClassName("active")[0].className = "non-active"
+  }
+  showError(){
+    document.getElementsByClassName("non-active")[0].className = "active"
+  }
 
 }
