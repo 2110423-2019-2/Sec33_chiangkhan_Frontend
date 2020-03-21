b0d5d878c22ee16503d3d8b9436433be9f76a583
Zongpol Jongkirkkiat <levatinez@gmail.com>
2020-03-21 14:40:07 +0700

    trying to get value from user input

--- src/app/listofcars/listofcars.component.ts
+++ src/app/listofcars/listofcars.component.ts
@@ -1,6 +1,8 @@
+import { FormGroup, FormControl } from '@angular/forms';
 import { RESERVATIONS } from './../reservations/mock-reservations';
 import { Component, OnInit } from '@angular/core';
 import { CARS } from './mock-cars';
+
 
 @Component({
   selector: 'app-listofcars',
@@ -10,6 +12,12 @@ import { CARS } from './mock-cars';
 
   export class ListofcarsComponent implements OnInit {
     
+    public user = [] ;
+    addDealForm = new FormGroup({
+      start_dates: new FormControl(),
+      pickup_location: new FormControl()
+    })
+
     constructor() { }
 
     ngOnInit() {
@@ -38,5 +46,29 @@ import { CARS } from './mock-cars';
     
     document.getElementById("form").className = "modal modal-fx-fadeInScale";
 }
+
+  startDateChange() {
+    // document.getElementById("start_dates")
+  }  
+
+  endDateChange() {
+    // document.getElementById("start_dates")
+  }  
+
+  pickupLocationChange() {
+
+  }
+
+  returnLocationChange() {
+
+  }
+
+  submit_add_deal() {
+    this.cars.unshift({ id: 50, image: "https://assets.bugatti.com/fileadmin/_processed_/sei/p54/se-image-e6678a2b1c56c59044f81a3742c784d4.jpg", 
+    Availability: 'Available', Type: 'Sport', Model: 'BMW i8', LicensePlate: 'พซ 1150', Seats: '5', Doors: '4',
+    StartDate: "someday", EndDate: '13/05/2019', PickupLocation: 'Suanlum', ReturnLocation: 'Suanlum', Price: '4,200' })
+    this.closeform();
+    console.log(this.addDealForm.value)
+  }
 
   }
