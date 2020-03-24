4ff0d479db6622d4a5ccea1a9c7ace8cf7648b5c
bosstyzaz <wutipong1234@hotmail.com>
2020-03-24 12:25:07 +0700

    show success register and alert when not fill out all of info

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -63,18 +63,47 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(){
+    if(this.registerForm.value.firstname == null || this.registerForm.value.lastname == null  || this.registerForm.value.username == null 
+      || this.registerForm.value.password == null  || this.registerForm.value.email == null || this.registerForm.value.phone_num == null
+      || this.registerForm.value.bank_account == null  || this.registerForm.value.bank_account_branch == null || this.registerForm.value.credit_card_number == null
+      || this.registerForm.value.credit_card_security == null  || this.registerForm.value.credit_card_expiry == null || this.registerForm.value.driving_license == null
+      || this.registerForm.value.address == null){
+      document.getElementsByClassName("non-active")[0].className = "active";
+    } else if(this.passwording !== this.confirmPassword) {
+      
+    }else {
     console.log(this.registerForm.value)
     axios.post('http://localhost:8080/api/users',this.registerForm.value)
       .then((response) => {
         console.log(response);
-        window.location.assign("/")
+        document.getElementById('registerpop').className = "modal is-active";
+        document.getElementsByClassName("active")[0].className = "non-active"
+        //window.location.assign("/")
       })
       .catch((error) => {
         console.log(error);
       });
+    }
   }
   cancel(){
     this.router.navigate(['/'])
+  }
+
+  del_registerpop() {
+    document.getElementById('registerpop').className = "modal";
+    window.location.assign("/");
+    console.log("work")
+  }
+
+  moveToLogin() {
+    window.location.assign("/");
+  }
+
+  hiddenError(){
+    document.getElementsByClassName("active")[0].className = "non-active"
+  }
+  showError(){
+    document.getElementsByClassName("non-active")[0].className = "active"
   }
   
 }
