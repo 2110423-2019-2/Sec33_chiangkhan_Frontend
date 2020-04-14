0b574a2113ab0b0a11a83c67e9cf05196bd4bdbf
Zongpol Jongkirkkiat <levatinez@gmail.com>
2020-04-14 14:02:50 +0700

    update verify edit profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+
+  editForm = new FormGroup({
+    first_name: new FormControl(),
+    last_name: new FormControl(),
+    email: new FormControl(),
+    phone_number: new FormControl(),
+    license_number: new FormControl(),
+    address: new FormControl(),
+  })
   
   constructor() {}
 
@@ -19,6 +29,40 @@ export class ProfileComponent implements OnInit {
     document.getElementsByClassName("modal modal-fx-fadeInScale is-active")[0].className = "modal modal-fx-fadeInScale"
   }
   updateProfile(){
-    //do somethings
+    console.log("update profile executed");
+    if (this.verify_edit_profile()) {
+      document.getElementById("first_name").nodeValue = this.editForm.get('first_name').value
+      this.closePopup()
+    }
+    
+  }
+
+  verify_edit_profile() {
+    console.log("executed verify submit");
+    if (this.editForm.get('first_name').value == null) {
+      console.log("first name is null!");
+      return false;
+    }
+    if (this.editForm.get('last_name').value == null) {
+      console.log("last name is null!");
+      return false;
+    }
+    if (this.editForm.get('email').value == null) {
+      console.log("email is null!");
+      return false;
+    }
+    if (this.editForm.get('phone_number').value == null) {
+      console.log("phone number is null!");
+      return false;
+    }
+    if (this.editForm.get('license_number').value == null) {
+      console.log("license number is null!");
+      return false;
+    }
+    if (this.editForm.get('address').value == null) {
+      console.log("address is null!");
+      return false;
+    }
+    return true;
   }
 }
